a32d22ff8fcd49bffa201b8b703c92ee1dd1efa7
montaglue <ev.pak.moc.liamg@gmail.com>
2023-03-14 21:25:53 +0300

    mail sending feature

--- users/src/handlers/waiting_list.rs
+++ users/src/handlers/waiting_list.rs
@@ -6,6 +6,11 @@ use mongodb::bson::oid::ObjectId;
 use serde::{Deserialize, Serialize};
 use utoipa::ToSchema;
 
+use lettre::{Message, SmtpTransport, Transport};
+use lettre::transport::smtp::authentication::Credentials;
+use check_if_email_exists::{check_email, CheckEmailInput, CheckEmailInputProxy, Reachable};
+
+
 use crate::error::Result;
 use crate::repositories::list_element::{ListElement, ListElementRepository};
 
@@ -18,6 +23,11 @@ pub struct PostElementRequest {
 pub struct PostElementResponse {
     pub id: String,
     pub email: String,
+}
+
+lazy_static::lazy_static! {
+    static ref EMAIL_ADDRESS: String = std::env::var("HELLO_MAIL_ADDRESS").unwrap();
+    static ref EMAIL_PASSWORD: String = std::env::var("HELLO_MAIL_PASSWORD").unwrap();
 }
 
 #[utoipa::path(
@@ -33,10 +43,32 @@ pub async fn post_element(
     Json(data): web::Json<PostElementRequest>,
     repo: web::Data<ListElementRepository>,
 ) -> Result<web::Json<PostElementResponse>> {
+    let email = data.email;
+
     let elem = ListElement {
         id: ObjectId::new(),
-        email: data.email,
+        email: email.clone(),
     };
+
+    let mut input = CheckEmailInput::new(email.clone());
+
+    let result = check_email(&input).await;
+
+    if result.is_reachable == Reachable::Safe || result.is_reachable == Reachable::Safe {
+        let email = Message::builder()
+            .from(EMAIL_ADDRESS.clone().parse().unwrap())
+            .to(email.clone().parse().unwrap())
+            .subject("Welcome to AuditDB waiting list!")
+            .body(include_str!("../../templates/waiting-letter.txt").to_string())
+            .unwrap();
+        let mailer = SmtpTransport::relay("smtp.gmail.com")
+            .unwrap()
+            .credentials(Credentials::new(EMAIL_ADDRESS.clone(), EMAIL_PASSWORD.clone()))
+            .build();
+        mailer.send(&email).unwrap();
+    }
+
+    
 
     repo.create(&elem).await?;
     return Ok(web::Json(PostElementResponse {
