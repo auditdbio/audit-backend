dc0337e966e1bddf662f9bb449c2353ea618a52f
montaglue <ev.pak.moc.liamg@gmail.com>
2023-03-15 18:16:16 +0300

    mongodb connection url fix

--- common/src/repository/mongo_repository.rs
+++ common/src/repository/mongo_repository.rs
@@ -21,7 +21,7 @@ impl<T> MongoRepository<T> {
     pub async fn new(mongo_uri: &str, database: &str, collection: &str) -> Self {
         Self {
             collection: mongodb::Client::with_uri_str(format!(
-                "mongodb://{}:{}@{}/?authSource=backend&authMechanism=SCRAM-SHA-256",
+                "mongodb://{}:{}@{}/?authMechanism=SCRAM-SHA-256",
                 MONGO_INITDB_ROOT_USERNAME.as_str(),
                 MONGO_INITDB_ROOT_PASSWORD.as_str(),
                 mongo_uri
