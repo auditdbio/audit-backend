0c9e67a4655bda707352e326a224569e045c9e03
montaglue <ev.pak.moc.liamg@gmail.com>
2023-03-25 10:31:23 +0300

    temporal query update

--- search/src/repositories/search.rs
+++ search/src/repositories/search.rs
@@ -37,11 +37,12 @@ impl SearchRepo {
     pub async fn find(&self, query: SearchQuery) -> Vec<Document> {
         let text = query.query + " " + &query.tags.join(" ");
 
-        let find_options = FindOptions::builder()
-            .sort(doc! {
-                query.sort_by: query.sort_order,
-            })
-            .build();
+        // let find_options = FindOptions::builder()
+        //     .sort(doc! {
+        //         query.sort_by: query.sort_order,
+        //     })
+        //     .build();
+        let find_options = None;
 
         let mut cursor = self
             .0
@@ -61,28 +62,28 @@ impl SearchRepo {
         let mut result = Vec::new();
         while let Some(doc) = cursor.next().await {
             let doc = doc.unwrap();
-            let tax_rate = doc.get_i64("tax_rate");
-            let time_from = doc.get_i64("time_from");
-            let time_to = doc.get_i64("time_to");
+            // let tax_rate = doc.get_i64("tax_rate");
+            // let time_from = doc.get_i64("time_from");
+            // let time_to = doc.get_i64("time_to");
             let ready_to_wait = doc.get_bool("ready_to_wait");
 
-            if let Ok(tax_rate) = tax_rate {
-                if tax_rate < query.tax_rate_from as i64 || tax_rate > query.tax_rate_to as i64 {
-                    continue;
-                }
-            }
+            // if let Ok(tax_rate) = tax_rate {
+            //     if tax_rate < query.tax_rate_from as i64 || tax_rate > query.tax_rate_to as i64 {
+            //         continue;
+            //     }
+            // }
 
-            if let Ok(time_from) = time_from {
-                if time_from < query.time_from as i64 {
-                    continue;
-                }
-            }
+            // if let Ok(time_from) = time_from {
+            //     if time_from < query.time_from as i64 {
+            //         continue;
+            //     }
+            // }
 
-            if let Ok(time_to) = time_to {
-                if time_to > query.time_to as i64 {
-                    continue;
-                }
-            }
+            // if let Ok(time_to) = time_to {
+            //     if time_to > query.time_to as i64 {
+            //         continue;
+            //     }
+            // }
 
             if let Ok(ready_to_wait) = ready_to_wait {
                 if ready_to_wait != query.ready_to_wait.unwrap_or(ready_to_wait) {
