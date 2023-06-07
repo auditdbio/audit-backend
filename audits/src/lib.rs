4bce3bb4cf0af8990ad7c6fe990653d664dc590d
montaglue <ev.pak.moc.liamg@gmail.com>
2023-06-07 12:30:16 +0300

    added method for issues

--- audits/src/lib.rs
+++ audits/src/lib.rs
@@ -45,7 +45,8 @@ pub fn create_app(
         .service(get_my_audit)
         .service(get_my_audit_request)
         .service(post_audit_issue)
-        .service(patch_audit_issue);
+        .service(patch_audit_issue)
+        .service(get_audit_issue);
 
     app
 }
