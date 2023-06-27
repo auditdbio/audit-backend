a5fb2e4298278d951dc28aa76509030f52d5325e
montaglue <ev.pak.moc.liamg@gmail.com>
2023-06-27 06:25:37 +0300

    added execution to report generation service

--- report/src/lib.rs
+++ report/src/lib.rs
@@ -1,2 +1,28 @@
+use actix_cors::Cors;
+use actix_web::{
+    body::MessageBody,
+    dev::{ServiceFactory, ServiceRequest, ServiceResponse},
+    middleware, App,
+};
+
 pub mod handlers;
 pub mod service;
+
+pub fn create_app() -> App<
+    impl ServiceFactory<
+        ServiceRequest,
+        Response = ServiceResponse<impl MessageBody>,
+        Config = (),
+        InitError = (),
+        Error = actix_web::Error,
+    >,
+> {
+    let cors = Cors::permissive();
+
+    #[allow(clippy::let_and_return)]
+    let app = App::new()
+        .wrap(cors)
+        .wrap(middleware::Logger::default())
+        .service(handlers::report::create_report);
+    app
+}
--- report/src/main.rs
+++ report/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::HttpServer;
+use report::create_app;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    env_logger::init();
+
+    HttpServer::new(create_app)
+        .bind(("0.0.0.0", 3011))?
+        .run()
+        .await
 }
