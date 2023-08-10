e9c6cdb886e5e64ff8df5e12328211edfc52e11a
Igor <63883555+samartsevigor@users.noreply.github.com>
2023-08-10 17:15:00 +0200

    added profile link & qr code

--- renderer/server/controllers/report/utils/createPageLinkAnnotation.js
+++ renderer/server/controllers/report/utils/createPageLinkAnnotation.js
@@ -1,7 +1,21 @@
-import { PDFName } from 'pdf-lib';
+import { PDFName, PDFString } from 'pdf-lib'
 
-const createPageLinkAnnotation = async (pdfDoc, tableOfContents) => {
+const createPageLinkAnnotation = async (pdfDoc, tableOfContents, profileLink) => {
   const pdfDocPages = pdfDoc.getPages()
+
+  const createProfileLinkAnnot = (profileLink) => {
+    return pdfDoc.context.register(
+      pdfDoc.context.obj({
+        Type: 'Annot',
+        Subtype: 'Link',
+        Rect: [60, 360, 150, 390],
+        Border: [0, 0, 0],
+        C: [1, 1, 1],
+        A: { Type: 'Action', S: 'URI', URI: PDFString.of(profileLink) },
+      }))
+  }
+
+  pdfDocPages[0].node.set(PDFName.of('Annots'), pdfDoc.context.obj([createProfileLinkAnnot(profileLink)]))
 
   const createAnnot = (pageRef, tocStringCoordY, destCoordY, tocStringNumberOfLines) => {
     return pdfDoc.context.register(
@@ -15,7 +29,7 @@ const createPageLinkAnnotation = async (pdfDoc, tableOfContents) => {
           tocStringCoordY + 8, // upper right y coord
         ],
         Border: [0, 0, 0], // Border for the link
-        C: [0, 0, 0], // Make the border color white: rgb(1, 1, 1)
+        C: [1, 1, 1], // Make the border color white: rgb(1, 1, 1)
         Dest: [pageRef, 'XYZ', null, destCoordY + 20, null], // Page to be visited when the link is clicked
       }))
   }
--- renderer/server/views/report/TitlePage.js
+++ renderer/server/views/report/TitlePage.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import HeroLogo from '../images/HeroLogo.js'
 import CornerLogo from '../images/CornerLogo.js'
+import QRCode from "qrcode.react"
 
 const TitlePage = ({ project }) => {
   return (
@@ -15,7 +16,14 @@ const TitlePage = ({ project }) => {
             <div className="auditor-info-block">
               <div className="auditor-info auditor-info-heading">By</div>
               <div className="auditor-info">{project.auditor_name}</div>
-              <div className="auditor-info">{project.auditor_email}</div>
+              <div className="QR-wrapper">
+                <QRCode.QRCodeSVG value={project.profile_link} />
+              </div>
+              <div>
+                <a className="auditor-info" href={project.profile_link}>
+                  Profile link
+                </a>
+              </div>
             </div>
           </div>
           <div className="col-6 hero-image-block">
