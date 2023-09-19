cc93948cd3d1cf9865f281b19c96183f9843f7c4
montaglue <ev.pak.moc.liamg@gmail.com>
2023-09-19 14:19:33 +0300

    added cache for dependencies

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,18 @@
 FROM rust:latest as build
+RUN cargo install cargo-chef
+WORKDIR /app
 
-WORKDIR /usr/src/audit_backend/
+
+FROM chef AS planner
 COPY . .
-RUN cargo build --release --features=test_server
+RUN cargo chef prepare --recipe-path recipe.json
+
+FROM chef AS builder
+COPY --from=planner /app/recipe.json recipe.json
+RUN cargo chef cook --release --recipe-path recipe.json
+
+COPY . .
+RUN cargo build --release
+
 RUN chmod +x ./setup.sh
 CMD ["./setup.sh"]
--- setup.sh
+++ setup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-cp /usr/src/audit_backend/target/release/users /data/binaries/users_binary
-cp /usr/src/audit_backend/target/release/customers /data/binaries/customers_binary
-cp /usr/src/audit_backend/target/release/swagger /data/binaries/swagger_binary
-cp /usr/src/audit_backend/target/release/auditors /data/binaries/auditors_binary
-cp /usr/src/audit_backend/target/release/audits /data/binaries/audits_binary
-cp /usr/src/audit_backend/target/release/files /data/binaries/files_binary
-cp /usr/src/audit_backend/target/release/search /data/binaries/search_binary
-cp /usr/src/audit_backend/target/release/mail /data/binaries/mail_binary
-cp /usr/src/audit_backend/target/release/notification /data/binaries/notification_binary
-cp /usr/src/audit_backend/target/release/event /data/binaries/event_binary
-cp /usr/src/audit_backend/target/release/report /data/binaries/report_binary
-cp /usr/src/audit_backend/target/release/chat /data/binaries/chat_binary
-cp /usr/src/audit_backend/target/release/telemetry /data/binaries/telemetry_binary
+cp /app/target/release/users /data/binaries/users_binary
+cp /app/target/release/customers /data/binaries/customers_binary
+cp /app/target/release/swagger /data/binaries/swagger_binary
+cp /app/target/release/auditors /data/binaries/auditors_binary
+cp /app/target/release/audits /data/binaries/audits_binary
+cp /app/target/release/files /data/binaries/files_binary
+cp /app/target/release/search /data/binaries/search_binary
+cp /app/target/release/mail /data/binaries/mail_binary
+cp /app/target/release/notification /data/binaries/notification_binary
+cp /app/target/release/event /data/binaries/event_binary
+cp /app/target/release/report /data/binaries/report_binary
+cp /app/target/release/chat /data/binaries/chat_binary
+cp /app/target/release/telemetry /data/binaries/telemetry_binary
