2357dabb99afad376e2dad0283c394df89628237
samartsevigor <i.samartsev@godeltech.com>
2023-11-22 13:54:47 +0600

    add api prefix

--- renderer/server/index.js
+++ renderer/server/index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3015
 app.use(cors())
 app.use(express.json())
 
-app.use(report)
+app.use('/api', report)
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`)
