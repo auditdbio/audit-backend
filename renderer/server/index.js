27325bb7c7a635b8727dc59e4b459dfb8b8d5ca4
samartsevigor <i.samartsev@godeltech.com>
2023-11-23 19:51:45 +0600

    error handling

--- renderer/server/index.js
+++ renderer/server/index.js
@@ -12,10 +12,6 @@ app.use(express.json())
 
 app.use('/api', report)
 
-app.use((err, req, res) => {
-  res.status(500).send("Server error")
-})
-
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`)
 })
