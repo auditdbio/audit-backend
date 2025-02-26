f903821af41e07e47ee3bb2665a33178cd7f7d1d
samartsevigor <i.samartsev@godeltech.com>
2025-02-26 20:25:36 +0600

    fixed filter with several tags

--- fts/src/models.rs
+++ fts/src/models.rs
@@ -65,49 +65,22 @@ fn deserialize_tags<'de, D>(deserializer: D) -> Result<Option<Vec<String>>, D::E
 where
     D: serde::Deserializer<'de>,
 {
-    struct TagsVisitor;
-
-    impl<'de> serde::de::Visitor<'de> for TagsVisitor {
-        type Value = Option<Vec<String>>;
-
-        fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
-            formatter.write_str("string or array of strings")
-        }
-
-        fn visit_str<E>(self, value: &str) -> Result<Self::Value, E>
-        where
-            E: serde::de::Error,
-        {
-            Ok(Some(vec![value.to_string()]))
-        }
-
-        fn visit_seq<S>(self, mut visitor: S) -> Result<Self::Value, S::Error>
-        where
-            S: serde::de::SeqAccess<'de>,
-        {
-            let mut values = Vec::new();
-            while let Some(value) = visitor.next_element()? {
-                values.push(value);
+    let value: Option<String> = Option::deserialize(deserializer)?;
+    
+    match value {
+        Some(s) => {
+            if s.contains(',') {
+                let tags: Vec<String> = s.split(',')
+                    .map(|s| s.trim().to_string())
+                    .filter(|s| !s.is_empty())
+                    .collect();
+                Ok(Some(tags))
+            } else {
+                Ok(Some(vec![s]))
             }
-            Ok(Some(values))
         }
-
-        fn visit_none<E>(self) -> Result<Self::Value, E>
-        where
-            E: serde::de::Error,
-        {
-            Ok(None)
-        }
-
-        fn visit_unit<E>(self) -> Result<Self::Value, E>
-        where
-            E: serde::de::Error,
-        {
-            Ok(None)
-        }
+        None => Ok(None),
     }
-
-    deserializer.deserialize_any(TagsVisitor)
 }
 
 impl SearchQuery {
